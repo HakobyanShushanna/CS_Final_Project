827d441519ad357741b5bda48c35ede24eaf2d0d
unknown <shushanna.hakobyan.rc@gmail.com>
2023-12-06 12:31:16 +0400

    Added Interface_Logic

---
+++ Interface_Logic/current_admin.py
@@ -0,0 +1,3 @@
+class Admin:
+    pass
+# TODO: implement class Admin
--- Interface_Logic/current_user.py
+++ Interface_Logic/current_user.py
@@ -1,2 +1,14 @@
 class User:
-    pass
+    def __init__(self, user):
+        self.first_name = user["First Name"]
+        self.last_name = user["Last Name"]
+        self.cards_ids = user["Cards Ids"]
+        self.email = user["Email"]
+        self.birth = user["Birth"]
+        self.passport = user["Passport No"]
+        self.password = user["Password"]
+        self.phone_number = user["Phone Number"]
+        self.currency = user["Preffered Currency"]
+        self.picture = user["Profile Picture"]
+        self.addresses = user["Addresses"]
+        self.balance = user["Balance"]
